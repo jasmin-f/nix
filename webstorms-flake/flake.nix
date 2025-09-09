974e906c4ca4cb7430ab9027cb2772ef91159dda
Jasmin <jasmin.faessler@ost.ch>
2025-09-10 00:21:00 +0200

    webstorm und node.js als devShell

--- webstorms-flake/flake.nix
+++ webstorms-flake/flake.nix
@@ -5,29 +5,32 @@
 
   outputs = { self, nixpkgs }: 
   
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { 
-      inherit system; 
-      config.allowUnfree = true;  # for webstorms
-      # # https://stackoverflow.com/questions/77585228/how-to-allow-unfree-packages-in-nix-for-each-situation-nixos-nix-nix-wit
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { 
+        inherit system; 
+        config.allowUnfree = true;  # for webstorms
+        # # https://stackoverflow.com/questions/77585228/how-to-allow-unfree-packages-in-nix-for-each-situation-nixos-nix-nix-wit
+      };
+
+
+      #   in rec {
+      #       # pkgs.webstorm;
+      #       pkgs.jetbrains.webstorm;
+      # });
+
+    in
+    {
+      
+      # syntax aus beginner workshop folien
+      devShells.${system} = {
+        nodejs = pkgs.mkShell {
+          packages = [ pkgs.nodejs ];
+        };
+        webstorm = pkgs.mkShell {
+          packages = [ pkgs.jetbrains.webstorm ];
+        };
+      };
+
     };
-
-    #   in rec {
-    #       # pkgs.webstorm;
-    #       pkgs.jetbrains.webstorm;
-    # });
-
-  in rec
-  {
-    
-    # aus Vorlage
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-    # packages.x86_64-linux.webstorm = nixpkgs.legacyPackages.x86_64-linux.jetbrains.webstorm;
-    # pkgs.jetbrains.webstorm;
-
-
-  };
 }
