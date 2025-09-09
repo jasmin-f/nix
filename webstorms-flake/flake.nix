7e9b38d0b12f1cb628267d38e99d55a10ba94fed
Jasmin <jasmin.faessler@ost.ch>
2025-09-10 00:30:47 +0200

    anderer syntax getestet und git-ignoriere webstorm dateien

--- webstorms-flake/flake.nix
+++ webstorms-flake/flake.nix
@@ -1,5 +1,6 @@
 {
-  description = "My first flake, webstorms for WE1";
+  description = "My first flake, webstorm for WE1
+  nix develop .#webstorm";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
@@ -7,30 +8,33 @@
   
     let
       system = "x86_64-linux";
+      # pkgs = nixpkgs.legacyPackages.${system};
+      # allowed-unfree-packages = [
+      #     "webstorm"
+      # ];
+      
+      # https://stackoverflow.com/questions/77585228/how-to-allow-unfree-packages-in-nix-for-each-situation-nixos-nix-nix-wit
       pkgs = import nixpkgs { 
-        inherit system; 
-        config.allowUnfree = true;  # for webstorms
-        # # https://stackoverflow.com/questions/77585228/how-to-allow-unfree-packages-in-nix-for-each-situation-nixos-nix-nix-wit
+        inherit system;
+        config = {
+          allowUnfree = true;  # for webstorm
+        };
       };
 
-
-      #   in rec {
-      #       # pkgs.webstorm;
-      #       pkgs.jetbrains.webstorm;
-      # });
-
+      
     in
     {
       
       # syntax aus beginner workshop folien
       devShells.${system} = {
-        nodejs = pkgs.mkShell {
-          packages = [ pkgs.nodejs ];
-        };
+        # nodejs = pkgs.mkShell {
+        #   packages = [ pkgs.nodejs ];
+        # };
         webstorm = pkgs.mkShell {
           packages = [ pkgs.jetbrains.webstorm ];
         };
       };
-
     };
+# Problem:
+# error: flake 'git+file:///mnt/c/Users/jf/code/wsl/nix?dir=webstorms-flake' does not provide attribute 'packages.x86_64-linux.webstorm', 'legacyPackages.x86_64-linux.webstorm' or 'webstorm'
 }
