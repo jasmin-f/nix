12291575eda4c263730264bfdda6185a6a85c5c3
Jasmin <jasmin.faessler@ost.ch>
2025-09-10 00:00:57 +0200

    url syntax angepasst

--- webstorms-flake/flake.nix
+++ webstorms-flake/flake.nix
@@ -1,31 +1,33 @@
 {
-  description = "A very basic flake";
+  description = "My first flake, webstorms for WE1";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: 
   
-  
-  {
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { 
+      inherit system; 
+      config.allowUnfree = true;  # for webstorms
+      # # https://stackoverflow.com/questions/77585228/how-to-allow-unfree-packages-in-nix-for-each-situation-nixos-nix-nix-wit
+    };
 
-    # # https://stackoverflow.com/questions/77585228/how-to-allow-unfree-packages-in-nix-for-each-situation-nixos-nix-nix-wit
-    # flake-utils.lib.eachDefaultSystem (system:
-    #   let
-    #     pkgs = import nixpkgs { 
-    #       inherit system;
-    #       config.allowUnfree = true;  # like this
-    #     };
     #   in rec {
-    #       # pkgs.someUnfreeThing is referenced somewhere in here
     #       # pkgs.webstorm;
     #       pkgs.jetbrains.webstorm;
     # });
 
+  in rec
+  {
+    
+    # aus Vorlage
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    # packages.x86_64-linux.webstorm = nixpkgs.legacyPackages.x86_64-linux.jetbrains.webstorm;
+    # pkgs.jetbrains.webstorm;
+
 
   };
 }
